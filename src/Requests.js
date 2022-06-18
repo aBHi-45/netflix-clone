5de3a3aabb481d945339a7a6d0623ef691107bfa
aBHi-45 <abhi287786@gmail.com>
2022-06-18 16:15:46 +0530

    Homescreen Netflix

---
+++ src/Requests.js
@@ -0,0 +1,15 @@
+//Typically we would store this in {process.env.API_KEY}
+const API_KEY = 'd40cf1fde64a581bd0a380edd1bd4074';
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genre=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genre=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genre=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genre=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genre=99`,
+};
+
+export default requests;
